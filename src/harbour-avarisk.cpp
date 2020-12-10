5bfa456050214fa044db421b387bc2cdadb5f641
fridlmue <fridl@10hoch-6.de>
2020-12-10 14:31:13 +0100

    add licence information GPL3

--- src/harbour-avarisk.cpp
+++ src/harbour-avarisk.cpp
@@ -1,3 +1,18 @@
+/*
+    Copyright (C) 2020 Friedrich Mütschele and other contributors
+    This file is part of avaRisk.
+    avaRisk is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    avaRisk is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with avaRisk. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifdef QT_QML_DEBUG
 #include <QtQuick>
 #endif
@@ -6,15 +21,5 @@
 
 int main(int argc, char *argv[])
 {
-    // SailfishApp::main() will display "qml/harbour-avarisk.qml", if you need more
-    // control over initialization, you can use:
-    //
-    //   - SailfishApp::application(int, char *[]) to get the QGuiApplication *
-    //   - SailfishApp::createView() to get a new QQuickView * instance
-    //   - SailfishApp::pathTo(QString) to get a QUrl to a resource file
-    //   - SailfishApp::pathToMainQml() to get a QUrl to the main QML file
-    //
-    // To display the view, call "show()" (will show fullscreen on device).
-
     return SailfishApp::main(argc, argv);
 }
